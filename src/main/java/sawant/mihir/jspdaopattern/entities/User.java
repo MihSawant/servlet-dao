a7609aadcf21adc3dd4b4cd71d89512cf2ea7f7b
Mihir Sawant <mihirssawant06@gmail.com>
2023-04-08 21:19:42 +0530

    modified User constructor with atts

--- src/main/java/sawant/mihir/jspdaopattern/entities/User.java
+++ src/main/java/sawant/mihir/jspdaopattern/entities/User.java
@@ -4,7 +4,10 @@ public class User {
     private String name;
     private int age;
 
-    public User(){};
+    public User(String name, int age){
+        this.name = name;
+        this.age = age;
+    };
 
     public String getName() {
         return name;
